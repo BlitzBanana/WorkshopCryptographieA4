90a6d9be4791ced4c2cd0df3b3482ce17fbf104f
Victor Rebiard--Crépin <crepinvcc@gmail.com>
2016-04-04 13:46:44 +0200

    Init Cryptography.CLI project

---
+++ Cryptography-CLI/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptography_CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commandManager = new Toolbox.CLI.CommandManager();
+
+            commandManager
+                .Command("sdes")
+                .Description("Descrypt a file with the given key.")
+                .Option("e,encrypt")
+                .Option("d,decrypt")
+                .Option("k,key")
+                .Option("i,in")
+                .Option("o,out")
+                .Action((callback, options) =>
+                {
+                    if (options.ContainsKey("decrypt") && !options.ContainsKey("encrypt"))
+                    {
+                        Program.Decrypt();
+                    }
+                    else if (options.ContainsKey("encrypt") && !options.ContainsKey("decrypt"))
+                    {
+                        Program.Encrypt();
+                    }
+                    else
+                    {
+                        commandManager.Error("Please use --decrypt OR --encrypt option.");
+                    }
+                });
+
+            commandManager
+                .Init(() =>
+                {
+                    commandManager.Log("Started interactive Cryptography CLI.");
+                })
+                .Delimiter("crypto:")
+                .Show();
+        }
+
+        static void Decrypt()
+        {
+
+        }
+
+        static void Encrypt()
+        {
+
+        }
+    }
+}
